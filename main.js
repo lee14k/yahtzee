ec75a16fbfa1d9fdb632b86969a2aae8e44f830d
lee14k <kmkovach@mtu.edu>
2022-10-26 15:16:48 -0400

    working on game logic

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ let dice=[]
 
 let rollButton = document.getElementById('roll').addEventListener('click', getRoll)
 
+let rolls=0;
+
 
 function getRoll () {
+    if (rolls===0) {
     let diceA = (Math.trunc(Math.random()*6)+1)
     let diceB = (Math.trunc(Math.random()*6)+1)
     let diceC = (Math.trunc(Math.random()*6)+1)
@@ -32,7 +35,14 @@ function getRoll () {
     three.innerText=dice[2]
     four.innerText=dice[3]
     five.innerText=dice[4]
+rolls++
+}else{
+    dice=[
+        
+    ]
 
+}
+//i only want to select the dice that the user has not moved into the saved dice array and create random numbers for those
 }
 
 console.log(positions)
@@ -40,15 +50,19 @@ console.log(positions)
 positions.forEach(item => {
     item.addEventListener('click', e =>{
             savedDice.push(item.innerText)
-            
+            dice.shift(item)
+            item.innerText=""
             savedDiceDisplay.innerText=savedDice
+            console.log(dice)
 
         //    console.log('moved')
            // console.log(dice)
-           console.log(savedDice)
+           //console.log(savedDice)
     })
 
     })
+
+//use current dice array length and only roll those dice 
 
     //how do i remove an element from the current roll and move it into the saved dice array
     //how do i add it back to the current roll array 
