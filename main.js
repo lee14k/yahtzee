bf2a6cf6584f39af9ce6022484df5113008737be
lee14k <kmkovach@mtu.edu>
2022-12-21 19:24:36 -0500

    improved functionality

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let savedDiceDisplay = document.getElementById('savedice')
 let savedDice = []
 let continuedDice
 let randomRoll
+let roundNumber
+let sum
+let savedDiceAgain
+let savedDiceAgainValues =[]
+let dude=[]
+let turtle
+let cola
 
 
 let one = document.getElementById('one')
@@ -11,12 +18,6 @@ let three = document.getElementById('three')
 let four = document.getElementById('four')
 let five = document.getElementById('five')
 
-let sone = document.getElementById('sone')
-let stwo = document.getElementById('stwo')
-let sthree = document.getElementById('sthree')
-let sfour = document.getElementById('sfour')
-let sfive = document.getElementById('sfive')
-
 let positions = [one, two, three, four, five]
 
 
@@ -24,11 +25,11 @@ let dice=[]
 
 let rollButton = document.getElementById('roll').addEventListener('click', getRoll)
 
-let rolls=0;
+let rolls=1;
 
 
 function getRoll () {
-    if (rolls===0) {
+    if (rolls===1 ||  rolls===3) {
     let diceA = (Math.trunc(Math.random()*6)+1)
     let diceB = (Math.trunc(Math.random()*6)+1)
     let diceC = (Math.trunc(Math.random()*6)+1)
@@ -42,15 +43,25 @@ function getRoll () {
     four.innerText=dice[3]
     five.innerText=dice[4]
 rolls++
-}else{
-    console.log(dice.length)
-   
+}else if(rolls===2){
+let diceA = (Math.trunc(Math.random()*6)+1)
+let diceB = (Math.trunc(Math.random()*6)+1)
+let diceC = (Math.trunc(Math.random()*6)+1)
+let diceD = (Math.trunc(Math.random()*6)+1)
+let diceE = (Math.trunc(Math.random()*6)+1)
 
+dice = [diceA, diceB, diceC, diceD, diceE]
+one.innerText=dice[0]
+two.innerText=dice[1]
+three.innerText=dice[2]
+four.innerText=dice[3]
+five.innerText=dice[4]
+rolls++
 }
+
 //i only want to select the dice that the user has not moved into the saved dice array and create random numbers for those
 }
 
-console.log(positions)
 
 positions.forEach(item => {
     item.addEventListener('click', e =>{
@@ -59,33 +70,79 @@ positions.forEach(item => {
             item.innerText=""
            // savedDiceDisplay.innerText=savedDice
            div = document.createElement('div')
-           div.setAttribute("id",'savey')
+           div.setAttribute("class",'savey')
+
+
+           console.log(savedDice)
+         console.log(positions)
+         console.log(dice)
+           
             savedDice.forEach( function  (index) {
                // div = div.cloneNode()
                 div.textContent=index
                 savedDiceDisplay.appendChild(div)
-             let turtle = document.getElementById('savey')  
-             turtle.addEventListener('click', addBack)
-function addBack () {
-    dice.push(50)
-    console.log(dice)
-}
+              //  console.log(index)
+                //dude.push(index)
+
+            })   
+            turtle = document.getElementsByClassName('savey')
+           // console.log(turtle)
+            cola=Array.from(turtle)
+            //console.log(dude)
+//destructure array using [first, second, third, fourth, fifth]
+//can set defaults to display 
+
+//dude = cola.values()
+  //          for (dude )
+
+
+            cola.forEach(el => {
+                el.addEventListener('click', items =>{
+                    //el.classList.toggle('disappear')
+                  //  console.log(savedDice)
+                    dice.push(parseInt(el.innerText))
+                    console.log(dice)
+                    positions.push(el)
+                    console.log(positions)
+                    divAgain=document.createElement('div')
+                    divAgain.textContent=el.innerText
+                    displayDice.appendChild(divAgain)
+                    savedDiceDisplay.removeChild(el)
+                    //console.log(positions)
             })
-            //sone.innerText=savedDice[0]
-            //stwo.innerText=savedDice[1]
-            //sthree.innerText=savedDice[2]
-            //sfour.innerText=savedDice[3]
-         
-        //    console.log('moved')
-           // console.log(dice)
-           //console.log(savedDice)
-    })
+            
 
-    })
+           // console.log(el.innerHTML)
 
+                })
 
+            })
+        })
+     
+  
+             
+           //for (i=0;i<turtle.length;i++) {
+             //console.log(cola[i].innerText)
+             //dude.push(cola[i].innerText) }
+          // console.log(dude)
+ 
+               //console.log(i)
+              // console.log(savedDiceAgain[i].textContent)
+            //  
+            //  
+              // console.log(savedDiceAgainValues)
+               // savedDiceAgain.push(turtle[i].innerText)
+          // }
+       //  console.log(dude)
+         // console.log(savedDiceAgainValues)
+           
+             //           console.log(items)
+//console.log(items.target.innerText)
+//console.log(savedDiceAgain)
+//console.log(items.target.textContent)                     
+             
+            
+            
+          //want to wait until saved dice are added back to save all values to array
 
-//use current dice array length and only roll those dice 
-
-    //how do i remove an element from the current roll and move it into the saved dice array
-    //how do i add it back to the current roll array 
+      
---
+++ scoring.js
@@ -0,0 +1,46 @@
+function aces() {
+
+}
+function twos() {
+
+}
+function threes(){
+
+}
+function fours(){
+
+}
+function fives(){
+
+}
+function sizes(){
+
+}
+
+
+
+function threeOfKind(){
+
+}
+function fourOfKind(){
+
+}
+function fullHouse(){
+
+}
+function sStraight(){
+
+}
+function lStraight(){
+
+}
+function yahtzee(){
+
+}
+function chance (){
+
+}
+
+function bonusYahtzee () {
+    
+}
