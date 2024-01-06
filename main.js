d992f496265270ac2f71c44346f5c844664b0e29
lee14k <kmkovach@mtu.edu>
2024-01-05 21:21:22 -0500

    working on basic functionality

--- main.js
+++ main.js
@@ -1,116 +1,69 @@
-let displayDice = document.getElementById('dicedisplay')
-let savedDiceDisplay = document.getElementById('savedice')
-let savedDice = []
-let continuedDice
-let randomRoll
-let roundNumber
-let sum
-let savedDiceAgain
-let savedDiceAgainValues =[]
-let dude=[]
-let turtle
-let cola
-let roundTwo = []
-let dice=[]
-let rollButton = document.getElementById('roll').addEventListener('click', getRoll)
-let positions = [one, two, three, four, five]
-let displayDiceLength 
+const displayDice = document.getElementById("display-dice");
+const savedDiceDisplay = document.getElementById("savedice");
+const rollButton = document.getElementById("roll");
+const positions = [one, two, three, four, five];
+const maxRolls = 3;
+let rolls = 0;
+let dice = [];
+rollButton.addEventListener("click", rollDice);
 
-let rolls=0;
-
-function generateDice () {
-  return (Math.trunc(Math.random()*6)+1)
+function generateDice() {
+  return Math.trunc(Math.random() * 6) + 1;
 }
-
-
-function removeElementsByClass(className){
-  const elements = document.getElementsByClassName('addedagain')
-  while(elements.length > 0){
-      elements[0].parentNode.removeChild(elements[0]);
+function rollDice() {
+  if (rolls >= maxRolls) {
+    return; // Limit the number of rolls per turn
   }
+console.log(rolls)
+  // Filter out the saved dice from the positions array
+  const remainingPositions = positions.filter((item) => !item.innerText);
+
+  // Roll only the remaining dice
+  remainingPositions.forEach((item, index) => {
+    const newValue = generateDice();
+    dice[index] = newValue;
+    item.innerText = newValue;
+  });
+
+  rolls++;
+
+  // Display the rolled dice
+  displayDiceValues();
 }
 
-function getRoll () {
- displayDiceLength = displayDice.children.length
-
-  if(displayDiceLength > 5){
-    console.log('tit')
-    let node = document.getElementsByClassName("addedagain");
-    let bat = Array.from(node)
-    console.log(bat)
-  
-    removeElementsByClass(bat)
-  
-    //toggle the display on nodes above to hidden
- // node.parentNode.removeChild(node);
-
-    let diceA = (Math.trunc(Math.random()*6)+1)
-    let diceB = (Math.trunc(Math.random()*6)+1)
-    let diceC = (Math.trunc(Math.random()*6)+1)
-    let diceD = (Math.trunc(Math.random()*6)+1)
-    let diceE = (Math.trunc(Math.random()*6)+1)
-    
-    
-    dice = [diceA, diceB, diceC, diceD, diceE]
-    one.innerText=dice[0]
-    two.innerText=dice[1]
-    three.innerText=dice[2]
-    four.innerText=dice[3]
-    five.innerText=dice[4]
-    
-       }
-  positions.forEach(item => {
-    item.innerText = generateDice()
+function displayDiceValues() {
+  positions.forEach((item,index)=>{
+    item.innerText=dice[index]
   })
-  
-        }
+}
 
-console.log(rolls)
-rolls++
+function saveDie(item,index){
+  const savedValue=parseInt(item.innerText);
+  dice.splice(index,1);
+  item.innerText="";
+  const savedDie=document.createElement('div')
+  savedDie.textContent=savedValue;
+  savedDie.classList.add('savey')
+  savedDiceDisplay.appendChild(savedDie);
+}
 
-positions.forEach(item => {
-    item.addEventListener('click', e =>{
-        div = document.createElement('div')
-        savedDiceDisplay.appendChild(div)
-        div.textContent=item.innerText
-        div.setAttribute("class",'savey')
-      
+function addBackDie(item,index){
+  const savedValue=parseInt(item.innerText);
+  dice.push(savedValue)
+  item.remove()
+  displayDiceValues()
+}
 
-        console.log(div)
-            savedDice.push(item.innerText)
-            console.log(savedDice)
-            item.innerText=""
-        
-           console.log(savedDiceDisplay)
-           
-           turtle = document.getElementsByClassName('savey')
-           cola=Array.from(turtle)
-           cola.forEach(el => {
-            console.log(el)
-            el.addEventListener('click', items =>{
-               savedDiceDisplay.removeChild(el)
-                //el.classList.toggle('disappear')
-              //  console.log(savedDice)
-                dice.push(parseInt(el.innerText))
-                positions.push(el)
-                divAgain=document.createElement('div')
-                divAgain.setAttribute("class", 'addedagain')
-                divAgain.textContent=el.innerText
-                displayDice.appendChild(divAgain)
-                
-                //console.log(positions)
-        })
+savedDiceDisplay.addEventListener('click',(e)=>{
+  if (e.target.classList.contains('savey')){
+    const index = Array.from(savedDiceDisplay.children).indexOf(e.target);
+    const savedDie=savedDiceDisplay.children[index];
+    addBackDie(savedDie,index)
+  }
+})
 
-    })
-
-      
-
-    console.log(cola)
-
-            
-
-                })
-
-            })
-
-    
+positions.forEach((item,index) => {
+  item.addEventListener("click", () => {
+    saveDie(item,index);
+  });
+});
